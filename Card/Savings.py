c2df917cd02034a192752132d2c5d8044c66b76f
Oleksandr Severhin <alexsevergin92@gmail.com>
2022-11-11 09:42:10 +0200

    Some minor changes in classes

---
+++ Card/Savings.py
@@ -0,0 +1,39 @@
+from Card.Card import Card
+
+#class that is responsible for checking card
+class Savings(Card):
+    id = 0
+    number = ""
+    password = ""
+    type = "savings"
+    balance = 0
+    gold = 0
+    userInterest = 0
+
+    #take money from the card
+    def withdraw(self, amount):
+        if (self.balance > amount):
+            self.balance -= amount
+        #else
+            #exceptions
+        # db call
+
+    #put money on the card
+    def putMoney(self, limit):
+        #some checks
+        self.limit = limit
+        #else
+            #exceptions
+
+    #changePassword
+    def changePassword(self, newPassword):
+        super()
+
+    #checkBalance
+    def checkBalance(self):
+        self()
+
+    #creates a new Card
+    def __init__(self, id, number, password, gold):
+        type = "credit"
+        super(self, id, number, password, type, gold)
