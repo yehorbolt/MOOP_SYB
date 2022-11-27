4c900954baf71198922a05f4416ceb0afcb299b4
Yehor Bolotov <Lollalka.007@gmail.com>
2022-11-27 02:03:35 +0200

    Minor changes of all class properties

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
 import mysql.connector
 import os
+
 db = mysql.connector.connect(
-  host="bunu9twon1zilnedmecc-mysql.services.clever-cloud.com",
-  user="uhdonzzoqdo00pjp",
-  password="qWKPUK6GPDJ5WaR2vYsG",
-  database="bunu9twon1zilnedmecc",
+    host="bunu9twon1zilnedmecc-mysql.services.clever-cloud.com",
+    user="uhdonzzoqdo00pjp",
+    password="qWKPUK6GPDJ5WaR2vYsG",
+    database="bunu9twon1zilnedmecc",
 )
 
 mycursor = db.cursor()
@@ -12,17 +13,15 @@ mycursor = db.cursor()
 mycursor.execute("SHOW DATABASES")
 
 for x in mycursor:
-  print(x)
+    print(x)
 
 print("-------------------------------------")
 mycursor.execute("show global variables like '%connections%'")
 
-
-
 for x in mycursor:
-  print(x)
+    print(x)
 
-#Основний файл з запуском гри, atm_start.py запускає нашу систему MonkePal
+# Основний файл з запуском гри, atm_start.py запускає нашу систему MonkePal
 
 # if __name__ == '__main__':
 #    print("Hello, my dear friend!");
