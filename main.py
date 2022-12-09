61961d0653aca4b325273c72afe8a3d084a4a3c7
Yehor Bolotov <Lollalka.007@gmail.com>
2022-12-09 23:23:21 +0200

    Updated GUI

--- main.py
+++ main.py
@@ -21,7 +21,7 @@ mycursor.execute("show global variables like '%connections%'")
 for x in mycursor:
     print(x)
 
-# Основний файл з запуском гри, atm_start.py запускає нашу систему MonkePal
+# Основний файл з запуском гри, atm_run.py запускає нашу систему MonkePal
 
 # if __name__ == '__main__':
 #    print("Hello, my dear friend!");
