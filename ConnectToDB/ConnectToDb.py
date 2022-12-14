41c0f1d0ee8e5991f6543f66b6614ff0a2fdb163
Yehor Bolotov <Lollalka.007@gmail.com>
2022-12-14 17:32:01 +0200

    Updated GUI

--- ConnectToDB/ConnectToDb.py
+++ ConnectToDB/ConnectToDb.py
@@ -4,26 +4,38 @@ from User.User import *
 from Account.Account import *
 from Card.Card import *
 
+connected = False
+connection = ""
+connection_config_dict= ""
 
 """
-Get the last id inserted in the database.
-:param: table name
-:type: str 
-:returns: number 
-:rtype: int 
+Connection to database.
 """
-def getLastId(table):
+def db_connect():
+    global connected
+    global connection
+    global connection_config_dict
+    connection_config_dict = {
+        'user': 'severhin1',
+        'password': 'AVNS_6rTR6l_ji_IFCMJbuSj',
+        'host': 'db-mysql-lon1-41175-do-user-12692486-0.b.db.ondigitalocean.com',
+        'port': '25060',
+        'database': 'defaultdb',
+    }
     try:
-        connection_config_dict = {
-            'user': 'severhin1',
-            'password': 'AVNS_6rTR6l_ji_IFCMJbuSj',
-            'host': 'db-mysql-lon1-41175-do-user-12692486-0.b.db.ondigitalocean.com',
-            'port': '25060',
-            'database': 'defaultdb',
-        }
         connection = mysql.connector.connect(**connection_config_dict)
+        connected = True
+    except Exception as e:
+        print(e)
 
-        if connection.is_connected():
+
+
+def getLastId(table):
+    global connected
+    global connection
+    try:
+
+        if connected:
             cursor = connection.cursor()
             query = "SELECT id FROM " + table + ";"
             cursor.execute(query)
@@ -32,14 +44,11 @@ def getLastId(table):
                 return 0
             else:
                 return records.__getitem__(-1)[-1]
+            connection.commit()
+            cursor.close()
 
     except Error as e:
         print("Error while connecting to MySQL", e)
-    finally:
-        if connection.is_connected():
-            connection.commit()
-            cursor.close()
-            connection.close()
 
 
 """
@@ -49,27 +58,18 @@ Execute the query in database
 :returns: nothing 
 """
 def execute(query):
+    global connected
+    global connection
     try:
-        connection_config_dict = {
-            'user': 'severhin1',
-            'password': 'AVNS_6rTR6l_ji_IFCMJbuSj',
-            'host': 'db-mysql-lon1-41175-do-user-12692486-0.b.db.ondigitalocean.com',
-            'port': '25060',
-            'database': 'defaultdb',
-        }
-        connection = mysql.connector.connect(**connection_config_dict)
 
-        if connection.is_connected():
+        if connected:
             cursor = connection.cursor()
             cursor.execute(query)
+            connection.commit()
+            cursor.close()
 
     except Error as e:
         print("Error while connecting to MySQL", e)
-    finally:
-        if connection.is_connected():
-            connection.commit()
-            cursor.close()
-            connection.close()
 
 
 """
@@ -79,27 +79,18 @@ Execute the query in database
 :returns: nothing 
 """
 def executeWithVal(query, values):
+    global connected
+    global connection
     try:
-        connection_config_dict = {
-            'user': 'severhin1',
-            'password': 'AVNS_6rTR6l_ji_IFCMJbuSj',
-            'host': 'db-mysql-lon1-41175-do-user-12692486-0.b.db.ondigitalocean.com',
-            'port': '25060',
-            'database': 'defaultdb',
-        }
-        connection = mysql.connector.connect(**connection_config_dict)
 
-        if connection.is_connected():
+        if connected:
             cursor = connection.cursor()
             cursor.execute(query, values)
+            connection.commit()
+            cursor.close()
 
     except Error as e:
         print("Error while connecting to MySQL", e)
-    finally:
-        if connection.is_connected():
-            connection.commit()
-            cursor.close()
-            connection.close()
 
 
 """
@@ -110,29 +101,20 @@ Execute the query in database and return the records from specific table
 :rtype: tuple
 """
 def executeReturn(query):
+    global connected
+    global connection
     try:
-        connection_config_dict = {
-            'user': 'severhin1',
-            'password': 'AVNS_6rTR6l_ji_IFCMJbuSj',
-            'host': 'db-mysql-lon1-41175-do-user-12692486-0.b.db.ondigitalocean.com',
-            'port': '25060',
-            'database': 'defaultdb',
-        }
-        connection = mysql.connector.connect(**connection_config_dict)
 
-        if connection.is_connected():
+        if connected:
             cursor = connection.cursor()
             cursor.execute(query)
             records = tuple(cursor.fetchall())
+            connection.commit()
+            cursor.close()
+            return records
 
     except Error as e:
         print("Error while connecting to MySQL", e)
-    finally:
-        if connection.is_connected():
-            connection.commit()
-            cursor.close()
-            connection.close()
-            return records
 
 
 """
@@ -142,29 +124,20 @@ Execute the query in database and print the records from the specific table in t
 :returns: nothing
 """
 def executePrint(query):
+    global connected
+    global connection
     try:
-        connection_config_dict = {
-            'user': 'severhin1',
-            'password': 'AVNS_6rTR6l_ji_IFCMJbuSj',
-            'host': 'db-mysql-lon1-41175-do-user-12692486-0.b.db.ondigitalocean.com',
-            'port': '25060',
-            'database': 'defaultdb',
-        }
-        connection = mysql.connector.connect(**connection_config_dict)
 
-        if connection.is_connected():
+        if connected:
             cursor = connection.cursor()
             cursor.execute(query)
             records = cursor.fetchall()
             print("Data: ", records)
+            connection.commit()
+            cursor.close()
 
     except Error as e:
         print("Error while connecting to MySQL", e)
-    finally:
-        if connection.is_connected():
-            connection.commit()
-            cursor.close()
-            connection.close()
 
 """
 This method restores data about the User
@@ -173,32 +146,21 @@ This method restores data about the User
 :returns: User
 """
 def restoreUser(login):
-    data = ""
+    global connected
+    global connection
     try:
-        connection_config_dict = {
-            'user': 'severhin1',
-            'password': 'AVNS_6rTR6l_ji_IFCMJbuSj',
-            'host': 'db-mysql-lon1-41175-do-user-12692486-0.b.db.ondigitalocean.com',
-            'port': '25060',
-            'database': 'defaultdb',
-        }
-        connection = mysql.connector.connect(**connection_config_dict)
-
-        if connection.is_connected():
+        if connected:
             cursor = connection.cursor()
             query = "SELECT * FROM user WHERE login = '" + str (login) + "';"
             cursor.execute(query)
             record = tuple (cursor.fetchall())
             data = [record.__getitem__(0)[1], record.__getitem__(0)[2], record.__getitem__(0)[3], True]
+            connection.commit()
+            cursor.close()
+            return data
 
     except Error as e:
         print("Error while connecting to MySQL", e)
-    finally:
-        if connection.is_connected():
-            connection.commit()
-            cursor.close()
-            connection.close()
-            return data
 
 """
 This method restores data about the Account
@@ -206,32 +168,22 @@ This method restores data about the Account
 :returns: Account
 """
 def restoreAccount(user_id):
-    account = ""
+    global connected
+    global connection
     try:
-        connection_config_dict = {
-            'user': 'severhin1',
-            'password': 'AVNS_6rTR6l_ji_IFCMJbuSj',
-            'host': 'db-mysql-lon1-41175-do-user-12692486-0.b.db.ondigitalocean.com',
-            'port': '25060',
-            'database': 'defaultdb',
-        }
-        connection = mysql.connector.connect(**connection_config_dict)
 
-        if connection.is_connected():
+        if connected:
             cursor = connection.cursor()
             query = "SELECT * FROM account WHERE user_id = '" + str (user_id) + "';"
             cursor.execute(query)
             record = tuple (cursor.fetchall())
             account = Account(record.__getitem__(0)[1], record.__getitem__(0)[2], record.__getitem__(0)[4], user_id, True)
+            connection.commit()
+            cursor.close()
+            return account
 
     except Error as e:
         print("Error while connecting to MySQL", e)
-    finally:
-        if connection.is_connected():
-            connection.commit()
-            cursor.close()
-            connection.close()
-            return account
 
 """
 This method restores data about the cards 
@@ -240,17 +192,11 @@ This method restores data about the cards
 :rtype: tuple
 """
 def restoreCards(account_id):
+    global connected
+    global connection
     try:
-        connection_config_dict = {
-            'user': 'severhin1',
-            'password': 'AVNS_6rTR6l_ji_IFCMJbuSj',
-            'host': 'db-mysql-lon1-41175-do-user-12692486-0.b.db.ondigitalocean.com',
-            'port': '25060',
-            'database': 'defaultdb',
-        }
-        connection = mysql.connector.connect(**connection_config_dict)
 
-        if connection.is_connected():
+        if connected:
             cursor = connection.cursor()
             query = "SELECT * FROM card WHERE account_id = '" + str (account_id) + "';"
             cursor.execute(query)
@@ -270,12 +216,9 @@ def restoreCards(account_id):
                     from Card.Savings import Savings
                     c = Savings(password, type, account_id,  True)
                 cards.append(c)
+            connection.commit()
+            cursor.close()
+            return cards
 
     except Error as e:
         print("Error while connecting to MySQL", e)
-    finally:
-        if connection.is_connected():
-            connection.commit()
-            cursor.close()
-            connection.close()
-            return tuple (cards)
